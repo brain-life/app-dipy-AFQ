9b432e30031c2abee86dff22498e5af473c12244
Aman Arya <aman.arya524@gmail.com>
2017-07-24 15:56:29 -0400

    Update main.py
    remove optional HARDI data and tracking

--- main.py
+++ main.py
@@ -19,19 +19,12 @@ def main():
 	with open('config.json') as config_json:
 	    config = json.load(config_json)
 	
-	
-	dpd.fetch_stanford_hardi()
-
-	hardi_dir = op.join(fetcher.dipy_home, "stanford_hardi")
-	data_file = op.join(hardi_dir, "HARDI150.nii.gz")
-	data_bval = op.join(hardi_dir, "HARDI150.bval")
-	data_bvec = op.join(hardi_dir, "HARDI150.bvec")
 	#Paths to data
-	"""
+	
 	data_file = str(config['data_file'])
 	data_bval = str(config['data_bval'])
 	data_bvec = str(config['data_bvec'])
-	"""
+	
 	img = nib.load(data_file)
 
 	print('Loaded Data')	
@@ -41,17 +34,14 @@ def main():
     		dti_params = dti.fit_dti(data_file, data_bval, data_bvec,out_dir='.')
 	else:
     		dti_params = {'FA': './dti_FA.nii.gz','params': './dti_params.nii.gz'}
-	#tg = nib.streamlines.load('track.trk').tractogram
-        streamlines = list(aft.track(dti_params['params']))
-	aus.write_trk('./HARDI_dti.trk', streamlines, affine=img.affine)
 	
-	"""
+	
 	tg = nib.streamlines.load(config['tck_data']).tractogram	
         #cannot remove inv, affine
 	streamlines = tg.apply_affine(np.linalg.inv(img.affine)).streamlines
         #streamlines = tg.streamlines       
         print('Loaded streamlines')
-        """
+        
 
 	# Use only a small portion of the streamlines, for expedience:
 	streamlines = streamlines[::100]
